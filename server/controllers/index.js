53fbc887c11dcdecc91ba54f63a00abed199429b
TanyaDimitrova <tanya.dimitrova@mail.bg>
2014-10-09 02:43:53 +0300

    Categories controller added

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,11 @@
 var usersController = require('../controllers/usersController');
 var carsController = require('../controllers/carsController');
 var makesController = require('../controllers/MakesController');
+var categoriesController = require('../controllers/CategoriesController');
 
 module.exports = {
     users: usersController,
     cars: carsController,
-    makes: makesController
+    makes: makesController,
+    categories: categoriesController
 };
